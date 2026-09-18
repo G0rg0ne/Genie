6b9a6745880df60c993835072e73d3c1e2b9fe6f
G0rg0ne <reda1999nox@gmail.com>
2026-09-18 23:41:34 +0200

    feat: :sparkles: integrate LLM functionality and agent graph creation

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 from utils.logging_config import setup_logging
 
 setup_logging()
-
-import langchain
-import langgraph
 from loguru import logger
+import os
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, MessagesState, START, END
+from langchain_core.messages import HumanMessage
+from dotenv import load_dotenv
 
-logger.info("Everything is ready!")
-logger.debug("LangChain and LangGraph are loaded")
+load_dotenv()
+MODEL_NAME = os.getenv("MODEL_NAME","gpt-4o-mini")
+
+def llm_call(state: MessagesState) -> MessagesState:
+    llm = ChatOpenAI(model=MODEL_NAME)
+    logger.info(f"Using: {MODEL_NAME} model for LLM call")
+    response = llm.invoke(state["messages"])
+    return MessagesState(messages=response.content)
+
+def agent_graph() -> StateGraph:
+    graph = StateGraph(MessagesState)
+    # add nodes to the graph
+    graph.add_node("llm_call", llm_call)
+
+    # add edges to the graph
+    graph.add_edge(START, "llm_call")
+    graph.add_edge("llm_call", END)
+
+    # return the graph
+    logger.info("Agent graph created")
+    return graph
+
+# create the graph
+graph = agent_graph().compile()
+
+# run the graph
+response = graph.invoke({"messages": [HumanMessage(content="Hello, how are you?")]})
+logger.info(f"Response: {response}")
