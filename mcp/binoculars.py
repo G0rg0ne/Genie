af18473ee4e01aeff6b80b342124bd479a3e248c
G0rg0ne <reda1999nox@gmail.com>
2026-09-24 01:46:59 +0200

    feat: :sparkles: add MCP service to docker-compose and implement new Dockerfile for deep search functionality

---
+++ mcp/binoculars.py
@@ -0,0 +1,28 @@
+import asyncio
+import json
+import sys
+from dataclasses import dataclass
+from datetime import datetime
+from typing import TypedDict
+
+from pydantic import BaseModel, Field
+
+from mcp.client import Client
+from mcp.server.mcpserver import MCPServer
+
+# Create server
+mcp = MCPServer("Deep search service")
+
+class SearchData(BaseModel):
+    """Structured search response"""
+    usefull_links: str = Field(description="link")
+
+@mcp.tool()
+def get_weather(city: str) -> SearchData:
+    """Get current weather for a city with full structured data"""
+    response = SearchData(usefull_links="www.google.com")
+    return response
+
+
+if __name__ == "__main__":
+    mcp.run(transport="sse", host="0.0.0.0", port=8080)
