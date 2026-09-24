f316515b37965ef2001d4a4c87d781e9ac1332a4
G0rg0ne <reda1999nox@gmail.com>
2026-09-25 00:55:29 +0200

    feat: :sparkles: update .dockerignore and .gitignore to exclude .env files, add env_file to docker-compose for mcp_server, and enhance search_mcp functionality with Firecrawl integration

--- mcp/search_mcp.py
+++ mcp/search_mcp.py
@@ -5,24 +5,28 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import TypedDict
 
+from firecrawl import Firecrawl
 from pydantic import BaseModel, Field
 
 from mcp.client import Client
 from mcp.server.mcpserver import MCPServer
+import os
 
+firecrawl_key = os.environ["FC_KEY"]
 # Create server
 mcp = MCPServer("Deep search service")
 
 class SearchData(BaseModel):
     """Structured search response"""
-    links: str = Field(description="link")
+    content_md: str = Field(description="the content of the scraped website")
 
 @mcp.tool()
-def get_link(topic: str) -> SearchData:
-    """Get current weather for a city with full structured data"""
-    response = SearchData(links="www.google.com")
+def scrap_link(link: str) -> SearchData:
+    """Get"""
+    scrap_app = Firecrawl(api_key=firecrawl_key)
+    result = scrap_app.scrape(link,formats=["markdown"])
+    response = SearchData(content_md=result)
     return response
-
 
 if __name__ == "__main__":
     mcp.run(transport="streamable-http", host="0.0.0.0", port=8080)
